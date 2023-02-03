0fd2ef528de60e78173e1bf49b2d615acf0a9ed5
kaxadlec <kaxadlec@gmail.com>
2023-02-03 14:13:15 +0900

    0203 linear list #6

--- day01.py
+++ day01.py
@@ -1,92 +1,58 @@
-# data structure_algorithm #day01-2
-
-def insert_data(idx, pokemon):
+# 함수 선언 부분
+def print_poly(px):
     """
-    선형 리스트의 idx위치에 원소 삽입
-    :param idx: int
-    :param pokemon:str
-    :return: void
+    다항식을 포멧에 맞게 출력하는 함수
+    :param px: 계수를 원소로 가지는 리스트
+    :return: 다항식 문자열
     """
-    if idx < 0 or idx > len(pokemons):
-        print("Out of range!")
-        return
+    term = len(px) - 1  # 최고차항 숫자 = 배열길이-1
+    poly_str = "P(x) = "
 
-    pokemons.append(None)  # 빈칸 추가
+    for i in range(len(px)):
+        coef = px[i]  # 계수
 
-    for i in range(len(pokemons) - 1, idx, -1):
-        pokemons[i] = pokemons[i - 1]
-        pokemons[i - 1] = None
+        # code 03-05.py
+        # if coef >= 0 :
+        #     poly_str = poly_str + "+"
 
-    pokemons[idx] = pokemon  # 지정한 위치에 친구 추가
-
-
-def delete_data(idx):
-    """
-    선형 리스트 idx 위치의 원소 삭제
-    :param idx: int
-    :return: str
-    """
-    if idx < 0 or idx > len(pokemons):
-        print("데이터를 삭제할 범위를 벗어났습니다.")
-        return
-
-    len_pokemons = len(pokemons)
-    pokemons[idx] = None  # 데이터 삭제
-
-    for i in range(idx + 1, len_pokemons):
-        pokemons[i - 1] = pokemons[i]
-        pokemons[i] = None  # 배열의 맨 마지막 위치 삭제
-
-    pokemons.pop()
-    # del (pokemons[len_pokemons - 1])
-
-    # self 3-1 professor
-    # for _ in range(len_pokemons - idx):
-    #     pokemons.pop()
-
-    # self 3-1 my code
-    # for n in range(1, len_pokemons):
-    #     del (pokemons[len_pokemons - n])
-
-
-def add_data(pokemon):
-    """
-    선형 리스트의 맨 뒤에 원소 삽입
-    :param pokemon:
-    :return:
-    """
-    pokemons.append(None)
-    pokemons[len(pokemons)-1] = pokemon
-
-
-pokemons = []
-menu = -1
-
-if __name__ == "__main__":
-    while True:
-
-        menu = input("선택하세요(1: 추가, 2: 삽입, 3: 삭제, 4: 종료)--> ")
-
-        if menu == '1':
-            data = input("추가할 데이터--> ")
-            add_data(data)
-            print(pokemons)
-        elif menu == '2':
-            pos = int(input("삽입할 위치--> "))
-            data = input("추가할 데이터--> ")
-            insert_data(pos, data)
-            print(pokemons)
-        elif menu == '3':
-            pos = int(input("삭제할 위치--> "))
-            delete_data(pos)
-            print(pokemons)
-        elif menu == '4':
-            print(pokemons)
-            # exit
-            break
-        else:
-            print("1~4 중 하나를 입력하세요.")
+        # self study 3-2
+        if i > 0 and coef > 0:
+            poly_str = poly_str + "+"
+        elif coef == 0:
+            term = term -1
             continue
 
+        poly_str = poly_str + f'{coef}x^{term} '
+        term = term - 1
+
+    return poly_str
 
 
+def calc_poly(x_val, p_x):
+    """
+    다항식의 산술연산을 하는 함수
+    :param x_val: x값 integer
+    :param p_x: 계수를 원소로 가지는 리스트
+    :return: 다항식 계산 결과값 integer
+    """
+    return_val = 0
+    term = len(p_x) - 1  # 최고차항 숫자 = 배열길이-1
+
+    for i in range(len(px)):
+        coef = p_x[i]  # 계수
+        return_val = return_val + coef * x_val ** term
+        term = term - 1
+
+    return return_val
+
+
+# 전역 변수 선언 부분
+px = [7, -4, 0, 5]  # = 7x^3 -4x^2 +0x^1 +5x^0
+
+
+# 메인 코드 부분
+if __name__ == "__main__":
+    p_str = print_poly(px)
+    print(p_str)
+    x_val = int(input("X 값-->"))
+    print(calc_poly(x_val, px))
