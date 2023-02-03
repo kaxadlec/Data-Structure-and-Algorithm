c0473b0eebf89b6aafc14538de72d60e760be9bc
kaxadlec <kaxadlec@gmail.com>
2023-02-03 11:17:50 +0900

    0203 linear list #2

--- day01.py
+++ day01.py
@@ -1,19 +1,25 @@
-# data structure_algorithm
+# data structure_algorithm #day01-2
 
-# pokemons = []  # 빈 배열
-pokemons = list()
+def insert_data(idx, pokemon):
+    if idx < 0 or idx > len(pokemons):
+        print("Out of range!")
+        return
+
+    pokemons.append(None)  # 빈칸 추가
+
+    for i in range(len(pokemons) - 1, idx, -1):
+        pokemons[i] = pokemons[i - 1]
+        pokemons[i - 1] = None
+
+    pokemons[idx] = pokemon  # 지정한 위치에 친구 추가
 
 
-def add_data(pokemon):
-    pokemons.append(None)
-    # pokemons[len(pokemons) - 1] = pokemon
-    pokemons[-1] = pokemon
+if __name__ == "__main__":
+    pokemons = ["피카츄", "라이츄", "파이리", "꼬부기", "버터풀"]
+    print(pokemons)
+    insert_data(2, '야도란')
+    print(pokemons)
+    insert_data(6, '피죤투')
+    print(pokemons)
 
 
-add_data('피카츄')
-add_data('라이츄')
-add_data('파이리')
-add_data('꼬부기')
-add_data('버터풀')
-
-print(pokemons)
