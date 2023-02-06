1af5aea056ae04513c4a2da9cc8e58a910c05018
kaxadlec <kaxadlec@gmail.com>
2023-02-06 13:14:15 +0900

    code #4-06.py

--- LinkedList.py
+++ LinkedList.py
@@ -1,16 +1,16 @@
 class Node():
-    def __init__(self):
+    def __init__(self):     # 노드 클래스 정의
         self.data = None
         self.link = None
 
 
-def print_nodes(start):
-    current = start
-    if current == None:
+def print_nodes(start):     # 단순 연결 리스트의 전체 노드 출력. # 매개변수는 시작노드 start
+    current = start         # 전달받은 시작노드를 현재 노드로 지정
+    if current == None:     # 노드에 데이터가 하나도 없는 연결 리스트인 경우 그냥 반환
         return
     print(current.data, end=' ')
 
-    while current.link != None:
+    while current.link != None:     # 노드의 링크가 빌 때까지 다음 노드를 계속 추적해서 출력
         current = current.link
         print(current.data, end=' ')
 
@@ -25,9 +25,39 @@ def print_nodes(start):
     #     print(current.data, end=' ')
 
 
-# memory = []
+def insert_node(find_data, insert_data):
+    global memory, head, current, pre
+
+    # 첫 번째에 노드 삽입
+    if head.data == find_data:
+        node = Node()
+        node.data = insert_data
+        node.link = head
+        head = node
+        return
+
+    # 중간에 노드 삽입
+    current = head
+    while current.link != None:
+        pre = current
+        current = current.link
+        if current.data == find_data:
+            node = Node()
+            node.data = insert_data
+            node.link = current
+            pre.link = node
+            return
+
+    # 마지막에 노드 삽입
+    node = Node()
+    node.data = insert_data
+    current.link = node
+
+
+memory = []
 head, current, pre = None, None, None
 data_array = ['가', '나', '다', '라', '마']
+
 
 if __name__ == "__main__":
     node = Node()   # 빈 노드 생성
@@ -42,7 +72,16 @@ if __name__ == "__main__":
         pre.link = node     # 기존 노드의 링크를 이번에 만든 노드에 연결
         # memory.append(node)
 
-print_nodes(head)
+    print_nodes(head)
+
+    insert_node("가","A")
+    print_nodes(head)
+
+    insert_node("다", "MID")
+    print_nodes(head)
+
+    insert_node("배열에 없는 문자", "END")
+    print_nodes(head)
 
 
 
