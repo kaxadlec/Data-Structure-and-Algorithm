d0df2970b9a14a813b58f423349ab7278f0de6ee
kaxadlec <kaxadlec@gmail.com>
2023-02-07 09:21:25 +0900

    Stack 06-09.py (Web page)

--- Stack.py
+++ Stack.py
@@ -1,68 +1,75 @@
+import webbrowser
+import time
+
+
+# 함수 선언 부분
 def is_stack_full():
-    global SIZE, stack, top     # 전역변수 참조
-    if top >= SIZE-1:
+    global SIZE, stack, top
+    if (top >= SIZE - 1):
         return True
-    # else:
-    return False
+    else:
+        return False
 
 
 def is_stack_empty():
-    global SIZE, stack, top  # 전역변수 참조
-    if top == - 1:
+    global SIZE, stack, top
+    if (top == -1):
         return True
-    # else:
-    return False
+    else:
+        return False
 
 
 def push(data):
-    global SIZE, stack, top  # 전역변수 참조
-    if is_stack_full():
-        print("Stack is Full")
+    global SIZE, stack, top
+    if (is_stack_full()):
+        print("스택이 꽉 찼습니다.")
         return
-    top = top + 1
+    top += 1
     stack[top] = data
 
-def pop():
-    global SIZE, stack, top  # 전역변수 참조
-    if is_stack_empty():
-        print("Stack is empty")
-        return
-    temp = stack[top]
-    stack[top] = None
-    top = top - 1
-    return temp
 
-def peek():
-    global SIZE, stack, top  # 전역변수 참조
-    if is_stack_empty():
-        print("Stack is empty")
+def pop():
+    global SIZE, stack, top
+    if (is_stack_empty()):
+        print("스택이 비었습니다.")
+        return None
+    data = stack[top]
+    stack[top] = None
+    top -= 1
+    return data
+
+def peek() :
+    global SIZE, stack, top
+    if (is_stack_empty()):
+        print("스택이 비었습니다.")
         return None
     return stack[top]
 
 
-SIZE = 5
-stack = [None for _ in range(SIZE)]     #list comprehension
+# 전역 변수 선언 부분
+SIZE = 100
+stack = [ None for _ in range(SIZE) ]
 top = -1
 
 
-if __name__ == "__main__":
-    while True:
-        menu = input("삽입(I) / 추출(E) / 확인(V) / 종료(X) 중 하나를 선택 ")
-        if menu == 'X' or menu == 'x':
-            break
-        elif menu == 'I' or menu == "i":
-            data = input("입력할 데이터: ")
-            push(data)
-            print("스택 상태: ", stack)
-        elif menu =="e":
-            data = pop()
-            print("추출한 데이터: ", data)
-            print("스택 상태: ", stack)
-        elif menu == 'v':
-            data = peek()
-            print("확인한 데이터: ", data)
-            print("스택 상태: ", stack)
-        else:`
-            print("다시 입력")
+# 메인 코드 부분
+if __name__ == "__main__" :
+    urls = ['youtube.com', 'google.com', 'music.youtube.com']
 
-    print("프로그램 종료")
+    for url in urls:
+        push(url)
+        webbrowser.open('http://' + url)
+        print(url, end='-->')
+        time.sleep(1)
+
+    print("방문 종료")
+    time.sleep(5)
+
+    while True:
+        url = pop()
+        if url == None:
+            break
+        webbrowser.open('http://' + url)
+        print(url, end='-->')
+        time.sleep(1)
+    print("방문 종료")
