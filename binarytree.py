5a3c7788d75fe81d927cea9436a6ea3398482ac1
kaxadlec <kaxadlec@gmail.com>
2023-02-07 17:04:25 +0900

    binarytree 08-3 (search_tree insert)

--- binarytree.py
+++ binarytree.py
@@ -1,74 +1,38 @@
-class TreeNode:
+class tree_node:
     def __init__(self):
-        self.left = None
         self.data = None
+        self.left = None
         self.right = None
 
+root = None
+club_array = ['첼시', '아스날', '토트넘', '맨유', '맨시티', '뉴캐슬', '리버풀']
 
-node1 = TreeNode()
-node1.data = '화사'
-
-node2 = TreeNode()
-node2.data = '솔라'
-node1.left = node2
-
-node3 = TreeNode()
-node3.data = '문별'
-node1.right = node3
-
-node4 = TreeNode()
-node4.data = '휘인'
-node2.left = node4
-
-node5 = TreeNode()
-node5.data = '쯔위'
-node2.right = node5
-
-node6 = TreeNode()
-node6.data = '선미'
-node3.left = node6
-
-node7 = TreeNode()
-node7.data = '다현'
-node4.right = node7
-
-node8 = TreeNode()
-node8.data = '사나'
-node6.right = node8
+# main
+node = tree_node()  # 트리 노드 생성
+node.data = club_array[0]   # 노드 데이터에 배열의 0번자리 데이터 넣음
+root = node     # 루트 노드 생성
 
 
-def preorder(node):
-    if node is None:
-        return
-    print(node.data, end='->')
-    preorder(node.left)
-    preorder(node.right)
+for fc in club_array[1:]:     # 클럽 배열의 1번자리부터 처리
 
+    current = root  # 항상 '첼시' = 루트 노드부터 시작. 현재 작업 노드 = 루트 노드
+    node = tree_node()  # 새 노드 생성
+    node.data = fc      # 클럽 배열 데이터를 노드에 대입
 
-def inorder(node):
-    if node is None:
-        return
-    inorder(node.left)
-    print(node.data, end='->')
-    inorder(node.right)
+    while True:
+        if fc < current.data:
+            if current.left is not None:
+                current = current.left
+            else:
+                current.left = node
+                break
 
+        elif fc > current.data:
+            if current.right is not None:
+                current = current.right
+            else:
+                current.right = node
+                break
 
-def postorder(node):
-    if node is None:
-        return
-    postorder(node.left)
-    postorder(node.right)
-    print(node.data, end='->')
+print("check")
 
-
-print('전위 순회 : ', end='')
-preorder(node1)
-print('끝')
-
-print('중위 순회 : ', end='')
-inorder(node1)
-print('끝')
-
-print('후위 순회 : ', end='')
-postorder(node1)
-print('끝')
