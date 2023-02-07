5a89ebeb3970d3eea73af9841690d8b444f769a1
kaxadlec <kaxadlec@gmail.com>
2023-02-07 23:12:59 +0900

    binarytree 08-06 list-list

--- binarytree.py
+++ binarytree.py
@@ -1,33 +1,43 @@
+import random
+
+
 class tree_node:
     def __init__(self):
         self.data = None
         self.left = None
         self.right = None
 
-root = None
-club_array = ['첼시', '아스날', '토트넘', '맨유', '맨시티', '리버풀']
 
-# main
+root_club, root_captain= None, None
+epl_array = [['첼시', '아스필리쿠에타'], ['아스날', '외데고르'], ['토트넘', '요리스'],
+              ['맨유', '매과이어'], ['맨시티', '귄도안'], ['리버풀', '헨더슨'],
+              ['뉴캐슬', '라셀레스'], ['웨스트햄', '라이스']]
+random.shuffle(epl_array)
+
+
+# club 트리
 node = tree_node()  # 트리 노드 생성
-node.data = club_array[0]   # 노드 데이터에 배열의 0번자리 데이터 넣음
-root = node     # 루트 노드 생성
+node.data = epl_array[0][0]   # 노드 데이터에 배열의 0번자리의 0번자리 club 데이터 넣음
+root_club = node     # 루트 노드 생성
 
 
-for fc in club_array[1:]:     # 클럽 배열의 1번자리부터 처리
+for epl in epl_array[1:]:     # epl 배열의 1번자리부터 처리
 
-    current = root  # 항상 '첼시' = 루트 노드부터 시작. 현재 작업 노드 = 루트 노드
+    current = root_club  # 현재 작업 노드 = 루트 노드
+
+    club = epl[0]
     node = tree_node()  # 새 노드 생성
-    node.data = fc      # 클럽 배열 데이터를 노드에 대입
+    node.data = club     # epl 배열의 0번방 club을 노드에 대입
 
     while True:
-        if fc < current.data:
+        if club < current.data:
             if current.left is not None:
                 current = current.left
             else:
                 current.left = node
                 break
 
-        elif fc > current.data:
+        elif club > current.data:
             if current.right is not None:
                 current = current.right
             else:
@@ -35,50 +45,60 @@ for fc in club_array[1:]:     # 클럽 배열의 1번자리부터 처리
                 break
 
 
-delete_fc = '토트넘'
+# captain 트리
+node = tree_node()
+node.data = epl_array[0][1]
+root_captain = node
+
+
+for epl in epl_array[1:]:     # epl 배열의 1번자리부터 처리
+
+    current = root_captain  # 현재 작업 노드 = 루트 노드
+
+    captain = epl[1]
+    node = tree_node()  # 새 노드 생성
+    node.data = captain     # epl 배열의 1번방 captain을 노드에 대입
+
+    while True:
+        if captain < current.data:
+            if current.left is not None:
+                current = current.left
+            else:
+                current.left = node
+                break
+
+        elif captain > current.data:
+            if current.right is not None:
+                current = current.right
+            else:
+                current.right = node
+                break
+
+club_or_captain = int(input('검색하고 싶은 목록을 선택하세오. 클럽---1 주장---2   '))
+find_epl = input('검색어를 입력하세요.')
+
+if club_or_captain == 1:
+    root = root_club
+elif club_or_captain == 2:
+    root = root_captain
 
 current = root
-parent = None      # 삭제할 노드의 상위 노드를 저장할 변수 선언
 
 while True:
-    if delete_fc == current.data:
-        if current.left is None and current.right is None:
-            if parent.left == current:
-                parent.left = None
-            elif parent.right == current:
-                parent.right = None
-            del(current)
-
-        elif current.left is not None and current.right is None:
-            if parent.left == current:
-                parent.left = current.left
-            elif parent.right == current:
-                parent.right = current.left
-            del(current)
-
-        elif current.left is None and current.right is not None:
-            if parent.left == current:
-                parent.left = current.right
-            elif parent.right == current:
-                parent.right = current.right
-            del(current)
-
-        print(f'{delete_fc} 삭제됨')
+    if find_epl == current.data:
+        print(f'{find_epl}은 목록에 있습니다.')
         break
 
-    elif delete_fc < current.data:
+    elif find_epl < current.data:
         if current.left is None:
-            print(f'{delete_fc}는 트리에 없습니다')
+            print(f'{find_epl}은 목록에 없습니다ㅠ')
             break
         else:
-            parent = current
             current = current.left
 
-    elif delete_fc > current.data:
+    elif find_epl > current.data:
         if current.right is None:
-            print(f'{delete_fc}는 트리에 없습니다')
+            print(f'{find_epl}은 목록에 없습니다ㅠ')
             break
         else:
-            parent = current
             current = current.right
-
