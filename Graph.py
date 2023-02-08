e1bfa89380e99e4643b966dc8d9dbdd4f8952f6c
kaxadlec <kaxadlec@gmail.com>
2023-02-08 18:17:09 +0900

    DFC Remaster

--- Graph.py
+++ Graph.py
@@ -3,37 +3,50 @@ class Graph:
         self.size = size
         self.graph = [[0 for i in range(size)] for i in range(size)]
 
+cfc = None
+stack = []
+visited_array = []
 
-def print_graph(data):
-    print('        ', end= '')
-    for i in range(data.size):
-        print(cfc_array[i], end=' ')
-    print()
-    for n in range(data.size):
-        print(cfc_array[n], end='   ')
-        for k in range(data.size):
-            print(data.graph[n][k], end='      ')
-        print()
-    print()
-
-
-chelsea = None
-cfc_array = ['Kai   ', 'Mason ', 'Reece ', 'NGolo ', 'Enzo  ', 'Kepa  ', 'Wesley']
-Kai =0; Mason=1; Reece =2; NGolo=3; Enzo=4; Kepa=5; Wesley=6
-
-cfc_size= 7
-
-chelsea = Graph(cfc_size)
-chelsea.graph[0][1] =1; chelsea.graph[0][3] =1; chelsea.graph[0][4] =1
-chelsea.graph[1][0] =1; chelsea.graph[1][2] =1; chelsea.graph[1][6] =1
-chelsea.graph[2][1] =1; chelsea.graph[2][4] =1; chelsea.graph[2][5] =1
-chelsea.graph[3][0] =1; chelsea.graph[3][6] =1
-chelsea.graph[4][0] =1; chelsea.graph[4][2] =1
-chelsea.graph[5][2] =1; chelsea.graph[5][6] =1
-chelsea.graph[6][1] =1; chelsea.graph[6][3] =1; chelsea.graph[6][5] =1
+cfc = Graph(3)
+cfc.graph[0][1] = 1
+cfc.graph[0][2] = 1
+cfc.graph[1][2] = 1
+cfc.graph[2][0] = 1
+cfc.graph[2][1] = 1
+cfc.graph[1][0] = 1
 
 # 무방향 그래프 출력
-print_graph(chelsea)
+print("무방향그래프")
+for i in range(3):
+    for n in range(3):
+        print(cfc.graph[i][n], end=' ')
+    print()
+
+current = 0
+stack.append(current)
+visited_array.append(current)
+
+while len(stack) != 0:
+    next = None
+
+    for vertex in range(3):
+        if cfc.graph[current][vertex] == 1:
+            if vertex in visited_array:
+                pass
+            else:
+                next = vertex
+                break
+
+    if next is not None:
+        current = next
+        stack.append(current)
+        visited_array.append(current)
+
+    else:
+        current = stack.pop()
+
+for z in visited_array:
+    print(chr(ord('A')+z), end =' ')
 
 
 
