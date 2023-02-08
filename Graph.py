16d764b8ea90c32423ca231de2653cab4081d220
kaxadlec <kaxadlec@gmail.com>
2023-02-08 17:40:18 +0900

    undirection graph for Chelsea FC

--- Graph.py
+++ Graph.py
@@ -4,31 +4,38 @@ class Graph:
         self.graph = [[0 for i in range(size)] for i in range(size)]
 
 
-undi = None
-di = None
-
-undi = Graph(5)     # 무방향 그래프, 사이즈는 5
-undi.graph[0][1] = 1; undi.graph[0][3] = 1; undi.graph[0][4] = 1
-undi.graph[1][0] = 1; undi.graph[1][2] = 1; undi.graph[1][4] = 1
-undi.graph[2][1] = 1; undi.graph[2][3] = 1; undi.graph[2][4] = 1
-undi.graph[3][0] = 1; undi.graph[3][2] = 1
-undi.graph[4][0] = 1; undi.graph[4][1] = 1; undi.graph[4][2] = 1
-
-print("무방향 그래프입니다")
-for i in range(5):
-    for n in range(5):
-        print(undi.graph[i][n], end=' ')
+def print_graph(data):
+    print('        ', end= '')
+    for i in range(data.size):
+        print(cfc_array[i], end=' ')
+    print()
+    for n in range(data.size):
+        print(cfc_array[n], end='   ')
+        for k in range(data.size):
+            print(data.graph[n][k], end='      ')
+        print()
     print()
 
-di = Graph(4)
-di.graph[0][3] = 1; di.graph[1][3] = 1
-di.graph[1][2] = 1
 
-print("방향 그래프입니다")
-for i in range(4):
-    for n in range(4):
-        print(di.graph[i][n], end=' ')
-    print()
+chelsea = None
+cfc_array = ['Kai   ', 'Mason ', 'Reece ', 'NGolo ', 'Enzo  ', 'Kepa  ', 'Wesley']
+Kai =0; Mason=1; Reece =2; NGolo=3; Enzo=4; Kepa=5; Wesley=6
+
+cfc_size= 7
+
+chelsea = Graph(cfc_size)
+chelsea.graph[0][1] =1; chelsea.graph[0][3] =1; chelsea.graph[0][4] =1
+chelsea.graph[1][0] =1; chelsea.graph[1][2] =1; chelsea.graph[1][6] =1
+chelsea.graph[2][1] =1; chelsea.graph[2][4] =1; chelsea.graph[2][5] =1
+chelsea.graph[3][0] =1; chelsea.graph[3][6] =1
+chelsea.graph[4][0] =1; chelsea.graph[4][2] =1
+chelsea.graph[5][2] =1; chelsea.graph[5][6] =1
+chelsea.graph[6][1] =1; chelsea.graph[6][3] =1; chelsea.graph[6][5] =1
+
+# 무방향 그래프 출력
+print_graph(chelsea)
+
+
 
 
 
