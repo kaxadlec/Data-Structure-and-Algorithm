88506b3311169685ba6652800e642c6495191284
kaxadlec <kaxadlec@gmail.com>
2023-02-09 17:48:06 +0900

    insertion sort #1

--- Sort.py
+++ Sort.py
@@ -1,31 +1,18 @@
-import random
+def score_sort(array):
+    n = len(array)
+    for end in range(1,n):
+        for current in range(end, 0, -1):
+            if array[current-1][1] > array[current][1]:
+                array[current-1], array[current] = array[current], array[current-1]
+    return array
 
-def quickSort(ary):
-    global cnt
-    cnt += 1
-    n = len(ary)
-    if n <= 1:  # 정렬할 리스트의 개수가 1개 이하면
-        return ary
+score_array = [['James', 40], ['Mason', 70], ['Thiago', 80], ['Kai', 60], ['Trevoh',5]]
 
-    pivot = ary[n // 2]  # 기준값을 중간값으로 지정
-    leftAry, rightAry = [], []
+print('정렬 전 --> ' , score_array)
+score_array = score_sort(score_array)
+print('정렬 후 --> ', score_array)
 
-    for num in ary:
-        if num > pivot:
-            leftAry.append(num)
-        elif num < pivot:
-            rightAry.append(num)
+print('스코어별 조편성')
+for i in range(len(score_array)//2):
+    print(score_array[i][0], ':', score_array[len(score_array)-1-i][0])
 
-    return quickSort(leftAry) + [pivot] + quickSort(rightAry)
-
-
-cnt =0
-
-## 전역 변수 선언 부분 ##
-dataAry = [random.randint(0, 200) for _ in range(20)]
-
-## 메인 코드 부분 ##
-print('정렬 전 -->', dataAry)
-dataAry = quickSort(dataAry)
-print('정렬 후 -->', dataAry)
-print(f"## {cnt}회로 정렬완료")
